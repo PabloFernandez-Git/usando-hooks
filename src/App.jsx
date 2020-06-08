7d3d6f71569dc862ea27e8a405d9b124052e3c01
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-08 18:26:38 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class App extends Component {
+const App = () => {
 
-    state = { number: 10 }
+    // state = { number: 10 }
+    // const state = useState(0)
+    // const number = state[0]
+    // [state, ()setState]
+    const [number, setNumber] = useState(10)
 
-    handleIncrement = () => {
-        this.setState({
+
+    const handleIncrement = () => {
+        /* this.setState({
             number: this.state.number + 1
-        })
+        }) */
+        setNumber(number + 1)
     }
 
-    handleDecrement = () => {
-        this.setState({
+    const handleDecrement = () => {
+        /* this.setState({
             number: this.state.number - 1
-        })
+        }) */
+        setNumber(number - 1)
     }
 
-    handleReset = () => {
-        this.setState({
+    const handleReset = () => {
+        /* this.setState({
             number: 0
-        })
+        }) */
+        setNumber(0)
     }
 
-    render() {
-        return (
-            <>
-                <h1>Number: {this.state.number}</h1>
-                <button onClick={this.handleIncrement}>Increment</button>
-                <button onClick={this.handleDecrement}>Decrement</button>
-                <button onClick={this.handleReset}>Reset</button>
-            </>
-        );
-    }
+    return (
+        <>
+            <h1>Number: {number}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+            <button onClick={handleReset}>Reset</button>
+        </>
+    );
+    
 }
 
 App.defaultProps = {
@@ -41,17 +48,57 @@ App.defaultProps = {
 
 export default App;
 
+
 /*
-Hooks
+useState
 
-Hooks son una nueva característica en React 16.8. Estos te permiten usar el estado y otras características de React sin escribir una clase.
+1. No utilizamos clases; las reemplazamos por una funcion.
 
-Es una forma de trabajar mucho mas comoda porque no solo escribimos menos codigo sino que es mas intuitivo y facil de depurar.
+const App = () => {}
 
-Las clases siguen estando disponibles pero se recomienda para los nuevos desarrollos utilizar Hooks.
 
-Los conceptos sobre ciclo de vida y props siguen funcionando exactamente igual.
-Se utilizan de una forma distinta pero sus conceptos no cambian.
+2. Tenemos que utilizar el hook de estado, ese hook se llama 'useState'. 
+Todos los hooks comienzan con la palabra 'use' seguido de su funcion.
+'useState' nos permite utilizar un estado dentro de funciones.
+
+import React, { useState } from 'react'
+
+
+3. Para declarar un estado :
+
+const [number, setNumber] = useState(10)
+
+Esto devuelve un array con dos posiciones.
+Tendriamos un array con el estado y una funcion que equivaldria al setState.
+Esto es lo que tendriamos en el array que devuelve el utilizar la funcion useState(10).
+La forma mas normal de declarar un estado es destructurando esto:
+
+[state, ()setState]
+
+Por lo tanto lo que se suele hacer es destructurar el array de la siguiente manera: 
+
+const [number, setNumber] = useState(10)
+const [nombre de nuestro estado, funcion que nos va a servir para actualizar ese numero] = useState(10)
+
+En number tenemos guardado el numero '10' que es el estado por defecto.
+
+( De esta forma se declaraba con clases => state = { number: 10 } )
+
+
+4. Al no ser una clase no necesitamos la funcion render().
+El 'return' si es necesario!
+
+
+5. Las funciones deben estar declaradas usando una constante ('const')
+
+6. Los manejadores (handleIncrement) no necesitan el 'this' (ya que no usamos clases)
+
+7. Para actualizar el estado tenemos la funcion 'setNumber' que creamos previamente.
+Como parametro simplemente le tenemos que decir lo que tiene que hacer.
+
+const handleIncrement = () => {
+    setNumber(number + 1)
+}
 
 
 */
