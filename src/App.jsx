e7bfae1e1e32f6f1d0a7071453d01b50b5a09f00
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-09 16:45:07 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,58 @@
-import React, { useState } from 'react'
-import Form from './components/Form'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-    // const [number, setNumber] = useState(10)
-    // const [text, setText] = useState('')
+    const [date, setDate] = useState(new Date().toLocaleTimeString())
 
-    // const handleIncrement = () => {
-    //     setNumber(number + 1)
-    // }
-
-    // const handleDecrement = () => {
-    //     setNumber(number - 1)
-    // }
-
-    // const handleReset = () => {
-    //     setNumber(0)
-    // }
-
-    // const handleInput = (e) => {
-    //     setText(e.target.value)
-    // }
-
+    useEffect(() => {
+        //componentDidMount
+        console.log('MOUNTED')
+        //componentDidUpdate
+        const time = setInterval(() => setDate(new Date().toLocaleTimeString()), 1000)
+        //componentWillUnmount
+        return () => {
+            clearInterval(time)
+        }
+    })
 
     return (
-        <>
-            {/* <h1>Number: {number}</h1>
-            <button onClick={handleIncrement}>Increment</button>
-            <button onClick={handleDecrement}>Decrement</button>
-            <button onClick={handleReset}>Reset</button>
-            <p>Contenido del input {text}</p>
-            <input type="text" onChange={handleInput} /> */}
-            <Form />
-        </>
+        <h1>{date}</h1>
     );
 }
 
 
 export default App;
+
+
+/*
+useEffect
+
+Es el segundo hook mas usado en React.
+Se usa para emular el ciclo de vida que teniamos en las clases (component­Did­Mount, component­Did­Update y component­Will­Unmount)
+Es una funcion.
+
+
+1. Debemos importarlo como cualquier dependencia, componente o funcion para utilizarlo.
+
+
+2. Reemplazando a "componentDidUpdate"
+Utilizamos la funcion 'setDate' que tenemos para asignar/actualizar el estado de 'date'.
+Creamos un 'setInterval' para poder actualizar el estado cada 'x' periodo de tiempo.
+Usando una arrow function le pasamos el 'setDate' y utilizamos dos paramentros (primero el método 'new Date().toLocaleTimeString()' y segundo el intervalo).
+
+
+3. Reemplazando a "componentWillUnmount"
+
+En el punto anterior estamos creando un intervalo cada vez que montamos el componente; naturalmente esto es incorrecto y va a traer errores. 
+Para evitar que eso pase tenemos que desmontar el componente e indicar que limpie el intervalo.
+
+La emulacion del "componentWillUnmount" viene a traves de un 'return' y lo que devolvemos es una funcion.
+Dentro de esta funcion vamos a indicar lo que queremos que pase cuando el componente se desmonte. 
+
+
+return () => {
+    clearInterval(time)
+}
+
+
+
+*/
