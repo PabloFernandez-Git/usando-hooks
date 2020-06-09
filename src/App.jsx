766aee9e1a364cbb1c87bb45ea2eab85ace2c753
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-09 11:55:02 -0300

    useState con strings

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import React, { useState } from 'react'
 
 const App = ({ initialNumber = 23 }) => {
-
-    // state = { number: 10 }
-    // const state = useState(0)
-    // const number = state[0]
-    // [state, ()setState]
+    
     const [number = initialNumber, setNumber] = useState(10)
-
+    const [text, setText] = useState('')
 
     const handleIncrement = () => {
-        /* this.setState({
-            number: this.state.number + 1
-        }) */
         setNumber(number + 1)
     }
 
     const handleDecrement = () => {
-        /* this.setState({
-            number: this.state.number - 1
-        }) */
         setNumber(number - 1)
     }
 
     const handleReset = () => {
-        /* this.setState({
-            number: 0
-        }) */
         setNumber(0)
+    }
+
+    const handleInput = (e) => {
+        setText(e.target.value)
     }
 
     return (
@@ -36,38 +27,34 @@ const App = ({ initialNumber = 23 }) => {
             <button onClick={handleIncrement}>Increment</button>
             <button onClick={handleDecrement}>Decrement</button>
             <button onClick={handleReset}>Reset</button>
+            <p>Contenido del input {text}</p>
+            <input type="text" onChange={handleInput} />
         </>
     );
     
 }
 
-App.defaultProps = {
-    number: 0
-}
-
-
 export default App;
 
 
 /*
-defaultProps
+useState con strings
 
+A diferencia de cuando trabajamos con clases podemos crear varios estados, y de hecho se recomienda crear un estado distinto para cada variable.
 
-Podemos pasar a nuestra funcion como prop un valor desde el index.js :
+const [text, setText] = useState('')
 
-ReactDOM.render(<App initialNumber={100} />, document.getElementById('root'));
+<p>Contenido del input {text}</p>
+<input type="text" onChange={handleInput} />
 
-
-Podemos dar a nuestra funcion un estado inicial por si la prop no llega:
-
-const App = ({ initialNumber = 23 }) => {
-    const [number = initialNumber, setNumber] = useState(10)
+const handleInput = (e) => {
+    setText(e.target.value)
 }
 
-Si el useState no tuviera un valor, tomaria primero el valor que llega de 'initialNumber' (viene como prop desde index.js) pero si esta prop no llega toma el valor de '23' que es el valor que le damos dentro de la funcion.
 
-Con esto lo que estamos haciendo es decirle, si te llega la prop 'initialNumer' le asignas ese valor, y sino por defecto le asigna el valor de '23' que es el que esta en la funcion.
-
+Creamos la funcion utilizando/capturando el evento (e) y se lo pasamos como valor del 'setState' (usando 'e.target.value').
+Cuando el valor del input cambia el evento 'onChange' lo detecta y a traves del 'handleInput' modifica el valor del texto.
+El texto cambia su valor y se renderiza dentro del <p></p>
 
 
 */
